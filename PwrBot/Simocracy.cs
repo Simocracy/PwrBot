fde8c88d963ae3a909f758473f4e74911ede351a
gobo7793 <gerald.siegert@outlook.com>
2016-07-24 20:45:15 +0200

    Added class for meta stuff for simocracy

---
+++ PwrBot/Simocracy.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Simocracy.PwrBot
+{
+	class Simocracy
+	{
+
+		/// <summary>
+		/// Gibt Nachfolgerrelationen in Simocracy basierend auf Flaggenkürzeln zurück. Schema: Vorgänger -> Aktueller Staat (bzw. Nachfolger).
+		/// </summary>
+		public static Dictionary<string, string> GetSuccessorFlags()
+		{
+			var dic = new Dictionary<string, string>();
+
+
+
+			return dic;
+		}
+	}
+}
